4c07692dc4b159827c08da74ad8e1042ff0cc7ca
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-13 09:35:00 -0500

    Se realiza Búsqueda general en todas las colecciones de forma simultánea

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -2,13 +2,31 @@
 
 const {response} = require('express');
 
-const getTodo=(req,res=response)=>{
-    const busqueda=req.params.busqueda
+const Usuario=require('../models/usuario');
+const Medico=require('../models/medico');
+const Hospital=require('../models/hospital');
+
+const getTodo=async(req,res=response)=>{
+    const busqueda=req.params.busqueda;
+    const regex=new RegExp(busqueda,'i');
+
+
+
+    const[usuarios,medicos,hospitales]=await Promise.all([
+        await Usuario.find({nombre:regex }),
+        await Medico.find({nombre:regex}),
+        await Hospital.find({nombre:regex})
+    ]);
+
+
+
 
     res.json({
         ok:true,
-        msg:'getTodo',
-        busqueda
+        usuarios,
+        medicos,
+        hospitales
+      
 })
 
 }
