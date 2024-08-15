fd6a501e9d83b3b0ade443130938de169a2b1878
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-15 08:44:22 -0500

    Se realiza el eliminar Hospital

--- controllers/hospitales.js
+++ controllers/hospitales.js
@@ -80,11 +80,35 @@ const actualizarHospital = async (req, res = response) => {
 
  
 
-const borrarHospital=(req,res=response)=>{
-    res.json({
-        ok:true,
-        msg:'borrarHospital'
-    });
+const borrarHospital=async(req,res=response)=>{
+    const id  = req.params.id;
+ 
+
+    try {
+        const hospital = await Hospital.findById( id );
+
+        if ( !hospital ) {
+            return res.status(404).json({
+                ok: true,
+                msg: 'Hospital no encontrado por id',
+            });
+        }
+
+         
+
+     await Hospital.findByIdAndDelete( id);
+
+        res.json({
+            ok: true,
+            msg: 'Hospital eliminado'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 module.exports={
